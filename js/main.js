b34c633fc22227fdc77152e0208b4991f46338ba
Justin Eder <justineder@gmail.com>
2017-11-22 16:02:49 -0600

    eliminated Firefox error

--- js/main.js
+++ js/main.js
@@ -55,6 +55,8 @@ ios += "  </div>";
 ios += "</div>";
 
 var flashVersion = "";
+flashVersion +=
+  "<script type='text/javascript' src='js/swfobject.js'></script>";
 flashVersion += '<div id="safariContainer">';
 flashVersion += '	<div id="flashContent">';
 flashVersion += '		<a href="http://www.adobe.com/go/getflash">';
@@ -78,78 +80,60 @@ function appleCheck() {
 
   /* **** BEGIN CHECK **** */
 
-  if (DetectRTC.isWebRTCSupported === true) {
-    theContent = webRTCVersion;
-  } else if (isIos) {
+  if (isIos) {
     theContent = ios;
+  } else if (DetectRTC.isWebRTCSupported === true) {
+    theContent = webRTCVersion;
   } else {
+    // ---------- Flash Business (Safari) ----------
+    function flashRecorder() {
+      var swfVersionStr = "11.4.0";
+      // <!-- xiSwfUrlStr can be used to define an express installer SWF. -->
+      var xiSwfUrlStr = "";
+      var flashvars = {};
+      var params = {};
+      params.quality = "high";
+      params.bgcolor = "#ffffff";
+      params.play = "true";
+      params.loop = "false";
+      params.wmode = "window";
+      params.scale = "noborder";
+      params.menu = "true";
+      params.devicefont = "false";
+      params.salign = "";
+      params.allowscriptaccess = "sameDomain";
+      var attributes = {};
+      attributes.id = "AudioRecorder";
+      attributes.name = "AudioRecorder";
+      attributes.align = "middle";
+      swfobject.createCSS("html", "height:100%; background-color: #ffffff;");
+      swfobject.createCSS(
+        "body",
+        "margin:0; padding:0; overflow:hidden; height:100%;"
+      );
+      swfobject.embedSWF(
+        "AudioRecorder.swf",
+        "flashContent",
+        "400",
+        "290",
+        swfVersionStr,
+        xiSwfUrlStr,
+        flashvars,
+        params,
+        attributes
+      );
+    }
+
+    //---------- End Flash Business ----------
+
     flashRecorder();
     theContent = flashVersion;
   }
-
-  /* **** END CHECK **** */
-
-  // /* **** BEGIN OLD CHECK **** */
-
-  // if (isIos) {
-  //   theContent = ios;
-  // } else if (isSafari) {
-  //   // testing for WebRTC in Safari
-  //   // theContent = chrome;
-
-  //   // original code that renders flash on safari
-  //   flashRecorder();
-  //   theContent = safari;
-  // } else {
-  //   theContent = chrome;
-  // }
-  // /* **** END OLD CHECK **** */
 
   document.getElementById("root").innerHTML = theContent;
 }
 
 //---------- End Browser Support ----------
-
-// ---------- Flash Business (Safari) ----------
-function flashRecorder() {
-  var swfVersionStr = "11.4.0";
-  // <!-- xiSwfUrlStr can be used to define an express installer SWF. -->
-  var xiSwfUrlStr = "";
-  var flashvars = {};
-  var params = {};
-  params.quality = "high";
-  params.bgcolor = "#ffffff";
-  params.play = "true";
-  params.loop = "false";
-  params.wmode = "window";
-  params.scale = "noborder";
-  params.menu = "true";
-  params.devicefont = "false";
-  params.salign = "";
-  params.allowscriptaccess = "sameDomain";
-  var attributes = {};
-  attributes.id = "AudioRecorder";
-  attributes.name = "AudioRecorder";
-  attributes.align = "middle";
-  swfobject.createCSS("html", "height:100%; background-color: #ffffff;");
-  swfobject.createCSS(
-    "body",
-    "margin:0; padding:0; overflow:hidden; height:100%;"
-  );
-  swfobject.embedSWF(
-    "AudioRecorder.swf",
-    "flashContent",
-    "400",
-    "290",
-    swfVersionStr,
-    xiSwfUrlStr,
-    flashvars,
-    params,
-    attributes
-  );
-}
-
-//---------- End Flash Business ----------
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
--- js/recorderjs/recorder.js
+++ js/recorderjs/recorder.js
@@ -17,23 +17,22 @@ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFT
 DEALINGS IN THE SOFTWARE.
 */
 
-(function(window){
+(function(window) {
+  var WORKER_PATH = "js/recorderjs/recorderWorker.js";
 
-  var WORKER_PATH = 'js/recorderjs/recorderWorker.js';
-
-  var Recorder = function(source, cfg){
+  var Recorder = function(source, cfg) {
     var config = cfg || {};
     var bufferLen = config.bufferLen || 4096;
     this.context = source.context;
-    if(!this.context.createScriptProcessor){
-       this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    if (!this.context.createScriptProcessor) {
+      this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
     } else {
-       this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
+      this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
     }
 
     var worker = new Worker(config.workerPath || WORKER_PATH);
     worker.postMessage({
-      command: 'init',
+      command: "init",
       config: {
         sampleRate: this.context.sampleRate
       }
@@ -41,83 +40,80 @@ DEALINGS IN THE SOFTWARE.
     var recording = false,
       currCallback;
 
-    this.node.onaudioprocess = function(e){
+    this.node.onaudioprocess = function(e) {
       if (!recording) return;
       worker.postMessage({
-        command: 'record',
+        command: "record",
         buffer: [
           e.inputBuffer.getChannelData(0),
           e.inputBuffer.getChannelData(1)
         ]
       });
-    }
+    };
 
-    this.configure = function(cfg){
-      for (var prop in cfg){
-        if (cfg.hasOwnProperty(prop)){
+    this.configure = function(cfg) {
+      for (var prop in cfg) {
+        if (cfg.hasOwnProperty(prop)) {
           config[prop] = cfg[prop];
         }
       }
-    }
+    };
 
-    this.record = function(){
+    this.record = function() {
       recording = true;
-    }
+    };
 
-    this.stop = function(){
+    this.stop = function() {
       recording = false;
       //automatically download file after recording is stopped
+    };
 
-
-    }
-
-    this.clear = function(){
-      worker.postMessage({ command: 'clear' });
-    }
+    this.clear = function() {
+      worker.postMessage({ command: "clear" });
+    };
 
     this.getBuffers = function(cb) {
       currCallback = cb || config.callback;
-      worker.postMessage({ command: 'getBuffers' })
-    }
+      worker.postMessage({ command: "getBuffers" });
+    };
 
-    this.exportWAV = function(cb, type){
+    this.exportWAV = function(cb, type) {
       currCallback = cb || config.callback;
-      type = type || config.type || 'audio/wav';
-      if (!currCallback) throw new Error('Callback not set');
+      type = type || config.type || "audio/wav";
+      if (!currCallback) throw new Error("Callback not set");
       worker.postMessage({
-        command: 'exportWAV',
+        command: "exportWAV",
         type: type
       });
-    }
+    };
 
-    this.exportMonoWAV = function(cb, type){
+    this.exportMonoWAV = function(cb, type) {
       currCallback = cb || config.callback;
-      type = type || config.type || 'audio/wav';
-      if (!currCallback) throw new Error('Callback not set');
+      type = type || config.type || "audio/wav";
+      if (!currCallback) throw new Error("Callback not set");
       worker.postMessage({
-        command: 'exportMonoWAV',
+        command: "exportMonoWAV",
         type: type
       });
-    }
+    };
 
-    worker.onmessage = function(e){
+    worker.onmessage = function(e) {
       var blob = e.data;
       currCallback(blob);
-    }
+    };
 
     source.connect(this.node);
-    this.node.connect(this.context.destination);   // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
+    this.node.connect(this.context.destination); // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
   };
 
-  Recorder.setupDownload = function(blob, filename){
+  Recorder.setupDownload = function(blob, filename) {
     var url = (window.URL || window.webkitURL).createObjectURL(blob);
     var link = document.getElementById("save");
     link.href = url;
-    link.download = filename || 'output.wav';
+    link.download = filename || "output.wav";
     console.log(link);
     autoDownload(link);
-  }
+  };
 
   window.Recorder = Recorder;
-
 })(window);
